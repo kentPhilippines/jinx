601a7cf231040a53ff46e42d88024b02ca826a38
kiwi <kiwi20200315@gmail.com>
2020-04-06 17:51:07 +0800

    【优化】 权限

--- ruoyi-framework/src/main/java/com/ruoyi/framework/web/service/PermissionService.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/web/service/PermissionService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 import com.ruoyi.common.utils.StringUtils;
 
 /**
- * RuoYi首创 js调用 thymeleaf 实现按钮权限可见性
+ * js调用 thymeleaf 实现按钮权限可见性
  * 
  * @author ruoyi
  */
