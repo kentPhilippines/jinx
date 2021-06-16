7a090324aa12b2214d5f6ed883836a5b4141a5bf
water <dev_water@lgv.com>
2021-06-16 17:13:34 +0800

    增加后台处理金额上限问题

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/alipay/AlipayMediumEntityController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/alipay/AlipayMediumEntityController.java
@@ -23,110 +23,135 @@ import com.ruoyi.common.core.page.TableDataInfo;
 
 /**
  * 收款媒介列Controller
- * 
+ *
  * @author kiwi
  * @date 2020-03-17
  */
 @Controller
 @RequestMapping("/alipay/medium")
 public class AlipayMediumEntityController extends BaseController {
-	private String prefix = "alipay/medium";
-	private String code_prefix = "alipay/file";
+    private String prefix = "alipay/medium" ;
+    private String code_prefix = "alipay/file" ;
 
-	@Autowired
-	private IAlipayMediumEntityService alipayMediumEntityService;
+    @Autowired
+    private IAlipayMediumEntityService alipayMediumEntityService;
 
-	@Autowired
-	private IAlipayFileListEntityService alipayFileListEntityService;
+    @Autowired
+    private IAlipayFileListEntityService alipayFileListEntityService;
 
-	@GetMapping()
-	public String medium() {
-		return prefix + "/medium";
-	}
-	/**
-	 * 查询收款媒介列列表
-	 */
-	@PostMapping("/list")
-	@ResponseBody
-	public TableDataInfo list(AlipayMediumEntity alipayMediumEntity) {
-		startPage();
-		List<AlipayMediumEntity> list = alipayMediumEntityService.selectAlipayMediumEntityList(alipayMediumEntity);
-		return getDataTable(list);
-	}
+    @GetMapping()
+    public String medium() {
+        return prefix + "/medium" ;
+    }
 
-	/**
-	 * 导出收款媒介列列表
-	 */
-	@Log(title = "收款媒介列", businessType = BusinessType.EXPORT)
-	@PostMapping("/export")
-	@ResponseBody
-	public AjaxResult export(AlipayMediumEntity alipayMediumEntity) {
-		List<AlipayMediumEntity> list = alipayMediumEntityService.selectAlipayMediumEntityList(alipayMediumEntity);
-		ExcelUtil<AlipayMediumEntity> util = new ExcelUtil<AlipayMediumEntity>(AlipayMediumEntity.class);
-		return util.exportExcel(list, "medium");
-	}
+    /**
+     * 查询收款媒介列列表
+     */
+    @PostMapping("/list")
+    @ResponseBody
+    public TableDataInfo list(AlipayMediumEntity alipayMediumEntity) {
+        startPage();
+        List<AlipayMediumEntity> list = alipayMediumEntityService.selectAlipayMediumEntityList(alipayMediumEntity);
+        return getDataTable(list);
+    }
 
-	/**
-	 * 新增收款媒介列
-	 */
-	@GetMapping("/add")
-	public String add() {
-		return prefix + "/add";
-	}
+    /**
+     * 导出收款媒介列列表
+     */
+    @Log(title = "收款媒介列", businessType = BusinessType.EXPORT)
+    @PostMapping("/export")
+    @ResponseBody
+    public AjaxResult export(AlipayMediumEntity alipayMediumEntity) {
+        List<AlipayMediumEntity> list = alipayMediumEntityService.selectAlipayMediumEntityList(alipayMediumEntity);
+        ExcelUtil<AlipayMediumEntity> util = new ExcelUtil<AlipayMediumEntity>(AlipayMediumEntity.class);
+        return util.exportExcel(list, "medium");
+    }
 
-	/**
-	 * 新增保存收款媒介列
-	 */
-	@Log(title = "收款媒介列", businessType = BusinessType.INSERT)
-	@PostMapping("/add")
-	@ResponseBody
-	public AjaxResult addSave(AlipayMediumEntity alipayMediumEntity) {
-		return toAjax(alipayMediumEntityService.insertAlipayMediumEntity(alipayMediumEntity));
-	}
+    /**
+     * 新增收款媒介列
+     */
+    @GetMapping("/add")
+    public String add() {
+        return prefix + "/add" ;
+    }
 
-	/**
-	 * 修改收款媒介列
-	 */
-	@GetMapping("/edit/{id}")
-	public String edit(@PathVariable("id") Long id, ModelMap mmap) {
-		AlipayMediumEntity alipayMediumEntity = alipayMediumEntityService.selectAlipayMediumEntityById(id);
-		mmap.put("alipayMediumEntity", alipayMediumEntity);
-		return prefix + "/edit";
-	}
+    /**
+     * 新增保存收款媒介列
+     */
+    @Log(title = "收款媒介列", businessType = BusinessType.INSERT)
+    @PostMapping("/add")
+    @ResponseBody
+    public AjaxResult addSave(AlipayMediumEntity alipayMediumEntity) {
+        return toAjax(alipayMediumEntityService.insertAlipayMediumEntity(alipayMediumEntity));
+    }
 
-	/**
-	 * 修改保存收款媒介列
-	 */
-	@Log(title = "收款媒介列", businessType = BusinessType.UPDATE)
-	@PostMapping("/edit")
-	@ResponseBody
-	public AjaxResult editSave(AlipayMediumEntity alipayMediumEntity) {
-		return toAjax(alipayMediumEntityService.updateAlipayMediumEntity(alipayMediumEntity));
-	}
+    /**
+     * 修改收款媒介列
+     */
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable("id") Long id, ModelMap mmap) {
+        AlipayMediumEntity alipayMediumEntity = alipayMediumEntityService.selectAlipayMediumEntityById(id);
+        mmap.put("alipayMediumEntity", alipayMediumEntity);
+        return prefix + "/edit" ;
+    }
 
-	/**
-	 * 删除收款媒介列
-	 */
-	@Log(title = "收款媒介列", businessType = BusinessType.DELETE)
-	@PostMapping("/remove")
-	@ResponseBody
-	public AjaxResult remove(String ids) {
-		return toAjax(alipayMediumEntityService.deleteAlipayMediumEntityByIds(ids));
-	}
+    /**
+     * 修改单个媒介限制金额
+     */
+    @PostMapping("/edit/{id}/{mountLimit}")
+    @ResponseBody
+    public AjaxResult modify(@PathVariable("id") Long id, @PathVariable("mountLimit") String mountLimit) {
+        AlipayMediumEntity alipayMediumEntity = new AlipayMediumEntity();
+        alipayMediumEntity.setId(id);
+        alipayMediumEntity.setMountLimit(mountLimit);
+        return toAjax(alipayMediumEntityService.updateAlipayMediumEntity(alipayMediumEntity));
+    }
+
+    /**
+     * 根据媒介类型更新限制金额
+     */
+    @PostMapping("/edit/{code}/{mountLimit}")
+    @ResponseBody
+    public AjaxResult modify(@PathVariable("code") String code, @PathVariable("mountLimit") String mountLimit) {
+        AlipayMediumEntity alipayMediumEntity = new AlipayMediumEntity();
+        alipayMediumEntity.setCode(code);
+        alipayMediumEntity.setMountLimit(mountLimit);
+        return toAjax(alipayMediumEntityService.updateAlipayMediumEntity(alipayMediumEntity));
+    }
+
+    /**
+     * 修改保存收款媒介列
+     */
+    @Log(title = "收款媒介列", businessType = BusinessType.UPDATE)
+    @PostMapping("/edit")
+    @ResponseBody
+    public AjaxResult editSave(AlipayMediumEntity alipayMediumEntity) {
+        return toAjax(alipayMediumEntityService.updateAlipayMediumEntity(alipayMediumEntity));
+    }
+
+    /**
+     * 删除收款媒介列
+     */
+    @Log(title = "收款媒介列", businessType = BusinessType.DELETE)
+    @PostMapping("/remove")
+    @ResponseBody
+    public AjaxResult remove(String ids) {
+        return toAjax(alipayMediumEntityService.deleteAlipayMediumEntityByIds(ids));
+    }
 
 
-	/**
-	 * 查看所属二维码列表
-	 */
-	@GetMapping("/show/{userId}")
-	public String showCodeList(@PathVariable("userId") String mediumId, ModelMap mmap) {
-		AlipayFileListEntity alipayFileListEntity = new AlipayFileListEntity();
-		alipayFileListEntity.setConcealId(mediumId);
-		alipayFileListEntity.setIsDeal("2");
-		List<AlipayFileListEntity> list = alipayFileListEntityService.selectAlipayFileListEntityList(alipayFileListEntity);
-		mmap.put("codeList", list);
-		return code_prefix + "/group_code_list";
-	}
+    /**
+     * 查看所属二维码列表
+     */
+    @GetMapping("/show/{userId}")
+    public String showCodeList(@PathVariable("userId") String mediumId, ModelMap mmap) {
+        AlipayFileListEntity alipayFileListEntity = new AlipayFileListEntity();
+        alipayFileListEntity.setConcealId(mediumId);
+        alipayFileListEntity.setIsDeal("2");
+        List<AlipayFileListEntity> list = alipayFileListEntityService.selectAlipayFileListEntityList(alipayFileListEntity);
+        mmap.put("codeList", list);
+        return code_prefix + "/group_code_list" ;
+    }
 
 
 }
