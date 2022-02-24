1866e76ab83559f8f4ef0519e892b83d2fb9acca
jacksmithphi@gmail.com <jacksmithphi@gmail.com>
2022-02-24 16:33:00 +0800

    //redis key加前缀

--- ruoyi-framework/src/main/java/com/ruoyi/framework/config/ShiroConfig.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/config/ShiroConfig.java
@@ -105,6 +105,9 @@ public class ShiroConfig {
     @Value("${spring.redis.password}")
     private String redisPassword;
 
+    @Value("${otc.appName}")
+    private String appName;
+
 
     /**
      * redisManager
@@ -132,7 +135,7 @@ public class ShiroConfig {
         redisCacheManager.setRedisManager(redisManager());
         // 选择属性字段作为缓存标识，这里选择account字段
         redisCacheManager.setPrincipalIdFieldName("userId");
-        redisCacheManager.setKeyPrefix("jinxin:"+RedisCacheManager.DEFAULT_CACHE_KEY_PREFIX);
+        redisCacheManager.setKeyPrefix(appName+":"+RedisCacheManager.DEFAULT_CACHE_KEY_PREFIX);
         // 设置信息缓存时间
         redisCacheManager.setExpire(86400);
         return redisCacheManager;
