fa56e6ad106117463f0697c05590bf968d931ce2
jacksmithphi@gmail.com <jacksmithphi@gmail.com>
2022-09-23 11:31:51 +0800

    //配置中心

--- ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -1,5 +1,7 @@
 package com.ruoyi;
 
+import com.alibaba.nacos.api.config.ConfigType;
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -11,6 +13,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  */
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableScheduling //轻量级定时任务
+@NacosPropertySource(dataId = "${nacos.config.data-id}",autoRefreshed = true)
 public class RuoYiApplication {
 	public static void main(String[] args) {
 //         System.setProperty("spring.devtools.restart.enabled", "false");
---
+++ ruoyi-admin/src/test/java/com/nacos/NacosTest.java
@@ -0,0 +1,51 @@
+package com.nacos;
+
+import com.alibaba.nacos.api.NacosFactory;
+import com.alibaba.nacos.api.PropertyKeyConst;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.config.listener.Listener;
+import com.alibaba.nacos.api.exception.NacosException;
+
+import java.util.Properties;
+import java.util.concurrent.Executor;
+
+public class NacosTest{
+
+    public static void main(String[] args) throws NacosException, InterruptedException {
+        String serverAddr = "47.242.206.196:8848";
+        String dataId = "admin2.yaml";
+        String group = "DEFAULT_GROUP";
+        Properties properties = new Properties();
+        properties.put(PropertyKeyConst.SERVER_ADDR, serverAddr);
+        ConfigService configService = NacosFactory.createConfigService(properties);
+        String content = configService.getConfig(dataId, group, 5000);
+        System.out.println(content);
+        configService.addListener(dataId, group, new Listener() {
+            @Override
+            public void receiveConfigInfo(String configInfo) {
+                System.out.println("recieve:" + configInfo);
+            }
+
+            @Override
+            public Executor getExecutor() {
+                return null;
+            }
+        });
+
+        boolean isPublishOk = configService.publishConfig(dataId, group, "content");
+        System.out.println(isPublishOk);
+
+        Thread.sleep(3000);
+        content = configService.getConfig(dataId, group, 5000);
+        System.out.println(content);
+
+        boolean isRemoveOk = configService.removeConfig(dataId, group);
+        System.out.println(isRemoveOk);
+        Thread.sleep(3000);
+
+        content = configService.getConfig(dataId, group, 5000);
+        System.out.println(content);
+        Thread.sleep(300000);
+
+    }
+}
